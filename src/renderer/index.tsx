ec4122cd29d0ae04490fffb37f2d65c3b80c54fc
savacan <sabacan.hige@gmail.com>
2019-11-28 16:28:57 +0900

    setup: eslint, prettier

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import { HelloWorld } from './src/components/hello';
+import {HelloWorld} from './src/components/hello';
 
 const App: React.FC = () => {
-    return (
-        <HelloWorld />
-    )
-}
+    return <HelloWorld />;
+};
 
-const contentDiv = document.getElementById('contents')
+const contentDiv = document.getElementById('contents');
 
 if (contentDiv) {
     ReactDom.render(<App />, contentDiv);
